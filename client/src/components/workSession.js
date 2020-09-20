b326a78dec12302e72d5135a2cc120eda9b68f27
Pignataro67 <Pignataro67@gmail.com>
2020-09-20 09:55:54 -0700

    adding comp.

---
+++ client/src/components/workSession.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import task from './workSessionComponent/task'
+
+class WorkSession extends Component {
+
+  render() {
+    return (
+      "Hellooooo"
+    )
+  }
+
+}
+
+export default WorkSession;
