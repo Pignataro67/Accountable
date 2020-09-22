4539e761e3a4042c98a04f98c0fdf97c9049d6ff
Pignataro67 <Pignataro67@gmail.com>
2020-09-22 07:42:53 -0700

    adding comp.

---
+++ client/src/sandbox.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import ReactCountdownClock from "react-countdown-clock";
+
+class Timer extends Component {
+  myCallback = () => {
+    return "Done";
+  };
+
+
+
+  render() {
+    
+    };
+
+    return 
+      "Hello"
+    ;
+  }
+}
+
+export default Timer;
